8df8c2d71e469ba119329e9a300428cb47bfe24c
Mike Han <qixinhan2015@u.northwestern.edu>
2015-02-08 16:35:42 -0600

    updated urls

--- agora/board/urls.py
+++ agora/board/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('board.views',
     # Examples:
     # url(r'^$', 'agora.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-   	url('^$', TemplateView.as_view(template_name='index.html')),
+   	url('^$', views.index),
 )
